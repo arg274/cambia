4c65c0aa0336ae77df3ad0cfc5c1f761cd13b325
arg274 <4648027+arg274@users.noreply.github.com>
2023-12-07 00:31:38 +0600

    Fix Grade border (again)

--- web/src/components/Grade.svelte
+++ web/src/components/Grade.svelte
@@ -6,7 +6,7 @@
 
     const opsEvaluations = evaluations.filter(x => x.evaluator === 'OPS');
 
-    let status: string = "surface";
+    let status: string;
     let statusGrade: string = "N/A";
 
     if (opsEvaluations.length > 0) {
@@ -33,13 +33,26 @@
             case (score == 100):
                 status = "success";
                 statusGrade = "S"
+                break;
             default:
+                status = "surface";
                 break;
         }
     }
 </script>
 <div class="relative overflow-hidden">
     <Card header="Grade" addClass="relative border-4 border-{status}-700 dark:border-{status}-500">
+        <!-- FIXME: PurgeCSS bug; I shouldn't have to explicitly mention these in the class(es) -->
+        <!-- https://github.com/FullHuman/purgecss/issues/1131 -->
+        <div class="hidden border-success-700 dark:border-success-500"></div>
+        <div class="hidden border-error-700 dark:border-error-500"></div>
+        <div class="hidden border-warning-700 dark:border-warning-500"></div>
+        <div class="hidden bg-success-400 dark:border-success-500"></div>
+        <div class="hidden bg-error-400 dark:border-error-500"></div>
+        <div class="hidden bg-warning-400 dark:border-warning-500"></div>
+        <div class="hidden bg-success-600 dark:border-success-500"></div>
+        <div class="hidden bg-error-600 dark:border-error-500"></div>
+        <div class="hidden bg-warning-600 dark:border-warning-500"></div>
         <div class="absolute top-4 w-1/4 h-2/3 bg-{status}-400 rounded-full filter blur-2xl opacity-50 dark:opacity-30 z-20 mix-blend-darken dark:mix-blend-color-dodge animate-blob"></div>
         <div class="absolute right-4 bottom-4 w-1/4 h-2/3 bg-{status}-600 rounded-full filter blur-2xl opacity-50 dark:opacity-30 z-20 mix-blend-darken dark:mix-blend-color-dodge animate-blob animation-delay-2000"></div>
         <span class="text-6xl font-black self-end">{statusGrade}</span>
--- web/src/routes/log/+page.svelte
+++ web/src/routes/log/+page.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
     import { browser } from '$app/environment';
-    import { fade } from 'svelte/transition';
 	import LogView from '../../components/LogView.svelte';
     
     import { page } from '$app/stores'
-	import { hexify } from '$lib/utils';
-	import { TableOfContents } from '@skeletonlabs/skeleton';
 	import type { CambiaResponse } from '$lib/types/CambiaResponse';
-	import { goto } from '$app/navigation';
 	import { hashIndexLookup, responseStore } from '$lib/LogStore';
 
     let logId: string | null;
@@ -26,17 +22,10 @@
 </script>
 
 {#if res}
-    <div class="mt-10 px-4 flex justify-center" id="single-rip-info" transition:fade={{duration: 150}}>
+    <!-- Transition bug: https://github.com/sveltejs/svelte/issues/544 -->
+    <div class="mt-10 px-4 flex justify-center" id="single-rip-info">
         <div class="w-full xl:w-3/4 2xl:w-1/2 md:max-lg:self-start">
             <LogView res={res} />
         </div>
-        <!-- <div class="hidden md:flex sticky top-14 self-start"> -->
-            <!-- FIXME: Currently scrolls to card header which is usually beneath AppBar -->
-            <!-- TODO: XSS vuln consideration: https://github.com/skeletonlabs/skeleton/issues/1987 -->
-            <!-- <TableOfContents
-                class="p-4"
-                regionLead="text-spaced-mini"
-                regionAnchor="text-spaced-mini" />
-        </div> -->
     </div>
 {/if}
