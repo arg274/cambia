823d73ff7539feca30c576fca4a3e8bb9614c5e8
arg274 <4648027+arg274@users.noreply.github.com>
2023-12-31 04:20:20 +0600

    Fix single log UI bug in /logs route

--- web/src/routes/logs/+page.svelte
+++ web/src/routes/logs/+page.svelte
@@ -6,17 +6,15 @@
 	import { onMount } from 'svelte';
 	import LogAggregate from '../../components/frags/LogAggregate.svelte';
 
-    const files = $fileListStore;
-
     onMount(() => {
-        if (!files || (files && files.length == 0)) {
+        if (!$fileListStore || ($fileListStore && $fileListStore.length == 0)) {
             goto('/');
         }
     });
 
 </script>
 
-{#if files && files.length > 1}
+{#if $fileListStore && $fileListStore.length > 1}
     <div class="flex justify-center">
         <div class="flex flex-col px-4 mt-10 w-full xl:w-3/4 2xl:w-1/2 md:max-lg:self-start gap-4">
             <LogAggregate />
