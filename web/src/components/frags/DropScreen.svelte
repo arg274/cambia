d96f8c7252aa5915a6ccad72a599845faf0d9ef7
arg274 <4648027+arg274@users.noreply.github.com>
2024-01-11 00:06:00 +0600

    Refactor DropScreen

--- web/src/components/frags/DropScreen.svelte
+++ web/src/components/frags/DropScreen.svelte
@@ -2,59 +2,21 @@
     import { createEventDispatcher } from 'svelte';
     import classNames from 'classnames';
     import IconCloudUpload from '~icons/carbon/cloud-upload';
-
-    export let value: string = '';
+    
     export let files: FileList | undefined;
 
     const dispatch = createEventDispatcher();
     let focused: boolean = false;
 
-    const baseClass: string = `
-        fixed
-        top-0
-        left-0
-        right-0
-        group
-        flex
-        flex-col
-        justify-center
-        items-center
-        w-screen
-        h-screen
-        z-max
-    `;
-    
-    const focusClass = `
-        visible ease-in duration-200
-        bg-surface-100-800-token
-    `;
-
-    const blurClass = `
-        invisible ease-out duration-200
-    `;
-
-    const textBaseClass = `
-        mt-2
-        mb-2
-    `;
-
+    const baseClass: string = "fixed top-0 left-0 right-0 group flex flex-col justify-center items-center w-screen h-screen z-max";
+    const focusClass = "visible ease-in duration-200 bg-surface-100-800-token";
+    const blurClass = "invisible ease-out duration-200";
+    const textBaseClass = "mt-2 mb-2";
     const textFocusClass = "";
-
-    const textBlurClass = `
-        invisible
-    `;
-
-    const svgBaseClass = `
-        mt-4
-        h-36
-        w-36
-    `;
-
+    const textBlurClass = "invisible";
+    const svgBaseClass = "mt-4 h-36 w-36";
     const svgFocusClass = "";
-
-    const svgBlurClass = `
-        hidden
-    `;
+    const svgBlurClass = "hidden";
     
     let input: HTMLInputElement;
 
@@ -101,7 +63,7 @@
         <IconCloudUpload class={classNames(svgBaseClass, focused ? svgFocusClass : svgBlurClass)} />
         <p class={classNames(textBaseClass, focused ? textFocusClass : textBlurClass)}><span class="font-bold">Drag and drop</span> log files here</p>
     </div>
-    <input {...$$restProps} bind:value bind:files bind:this={input} type="file" class="hidden" on:change on:click />
+    <input {...$$restProps} bind:files bind:this={input} type="file" class="hidden" on:change on:click />
 </label>
 <div class={focused ? classNames("hidden", "pointer-events-none") : "visible h-full"}>
     <slot />
