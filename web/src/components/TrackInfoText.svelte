e551debb0c5ad0a1bef157f02f27373db6ad8bef
arg274 <4648027+arg274@users.noreply.github.com>
2024-02-05 00:34:34 +0600

    Add guard for empty tracklist

--- web/src/components/TrackInfoText.svelte
+++ web/src/components/TrackInfoText.svelte
@@ -78,93 +78,95 @@
         gotoPage();
     }
 </script>
-<div>
-    <div class="flex justify-center md:justify-end md:items-center items-end gap-2 mb-2">
-        <Paginator bind:settings={page} on:page={onPageChange}></Paginator>
-        <div class="flex justify-end items-center hide-scroll-numinput">
-            <input type="number" required bind:value={inputPage} class="w-12 variant-filled py-1.5 text-center text-sm rounded-l-full" on:keypress={pageInputHandler} on:click|preventDefault={selectText} bind:this={inputEl} />
-            <button type="button" class="variant-filled py-1.5 px-2 rounded-r-full" on:click={gotoPage}><IconArrowRight /></button>
-        </div>
-    </div>
-    <div class="flex flex-col gap-4">
-        <div class="flex gap-2 items-center">
-            <h6>Track {tracks[page.page].num}</h6>
-            {#if tracks[page.page].aborted}
-                <div class="variant-soft-error rounded-md text-xs px-2 py-1">Aborted</div>
-            {/if}
+{#if tracks.length > 0}
+    <div>
+        <div class="flex justify-center md:justify-end md:items-center items-end gap-2 mb-2">
+            <Paginator bind:settings={page} on:page={onPageChange}></Paginator>
+            <div class="flex justify-end items-center hide-scroll-numinput">
+                <input type="number" required bind:value={inputPage} class="w-12 variant-filled py-1.5 text-center text-sm rounded-l-full" on:keypress={pageInputHandler} on:click|preventDefault={selectText} bind:this={inputEl} />
+                <button type="button" class="variant-filled py-1.5 px-2 rounded-r-full" on:click={gotoPage}><IconArrowRight /></button>
+            </div>
         </div>
         <div class="flex flex-col gap-4">
-            <InfoSegment icon={IconSplitScreen} header="Track splitting" value={tracks[page.page].is_range ? "Range" : "Split"} />
-            <InfoSegment icon={IconSidePanelOpenFilled} header="Extraction speed" value={`${tracks[page.page].extraction_speed?.toFixed(1)}x`} />
-            <InfoSegment icon={IconMountain} header="Peak level" value={tracks[page.page].peak_level?.toFixed(3)} />
-            <InfoSegment icon={IconMicrophone} header="Gain" value={tracks[page.page].gain} />
-            <InfoSegment icon={IconExpandCategories} header="Pregap length" value={tracks[page.page].pregap_length ? `${parseFloat(nonNullAssert(tracks[page.page].pregap_length)).toFixed(2)} sec` : null} />
-            <InfoSegment icon={IconTransmissionLte} header="Pre-emphasis" value={tracks[page.page].preemphasis} />
-        </div>
-        {#if tracks[page.page].filenames.length > 0}
-            <div class="flex flex-col gap-2">   
-                <div class="flex items-center"><IconDocumentBlank class="icon-sm" /><span class="ml-2 dark:font-light text-sm">Filename</span></div>
-                <!-- TODO: Only show the first filename for now -->
-                <!-- Leading slashes in *nix paths need to be trimmed to not mess up RTL -->
-                <div class="font-mono grow bg-surface-50-900-token px-2 py-1 col-span-9 truncate text-end" dir="rtl">{trimLeftChar(tracks[page.page].filenames[0], "/")}</div>
+            <div class="flex gap-2 items-center">
+                <h6>Track {tracks[page.page].num}</h6>
+                {#if tracks[page.page].aborted}
+                    <div class="variant-soft-error rounded-md text-xs px-2 py-1">Aborted</div>
+                {/if}
             </div>
-        {/if}
-        <hr class="!border-t-4 !border-dashed" />
-        <InfoSegment header="Integrity" value={tracks[page.page].test_and_copy.integrity} icon={IconDoubleInteger} />
-        <InfoSegment header="Integrity (skip zeroes)" value={tracks[page.page].test_and_copy.integrity_skipzero} icon={IconDoubleInteger} />
-        
-        {#if tracks[page.page].test_and_copy.integrity === 'Match' }
-            <ChecksumSegment header="T&C hash" hash={tracks[page.page].test_and_copy.test_hash} icon={IconHashtag} status={tracks[page.page].test_and_copy.integrity} />
-        {:else}
-            <ChecksumSegment header="Test hash" hash={tracks[page.page].test_and_copy.test_hash} icon={IconHashtag} status={tracks[page.page].test_and_copy.integrity} />
-            <ChecksumSegment header="Copy hash" hash={tracks[page.page].test_and_copy.copy_hash} icon={IconHashtag} status={tracks[page.page].test_and_copy.integrity} />
-        {/if}
+            <div class="flex flex-col gap-4">
+                <InfoSegment icon={IconSplitScreen} header="Track splitting" value={tracks[page.page].is_range ? "Range" : "Split"} />
+                <InfoSegment icon={IconSidePanelOpenFilled} header="Extraction speed" value={`${tracks[page.page].extraction_speed?.toFixed(1)}x`} />
+                <InfoSegment icon={IconMountain} header="Peak level" value={tracks[page.page].peak_level?.toFixed(3)} />
+                <InfoSegment icon={IconMicrophone} header="Gain" value={tracks[page.page].gain} />
+                <InfoSegment icon={IconExpandCategories} header="Pregap length" value={tracks[page.page].pregap_length ? `${parseFloat(nonNullAssert(tracks[page.page].pregap_length)).toFixed(2)} sec` : null} />
+                <InfoSegment icon={IconTransmissionLte} header="Pre-emphasis" value={tracks[page.page].preemphasis} />
+            </div>
+            {#if tracks[page.page].filenames.length > 0}
+                <div class="flex flex-col gap-2">   
+                    <div class="flex items-center"><IconDocumentBlank class="icon-sm" /><span class="ml-2 dark:font-light text-sm">Filename</span></div>
+                    <!-- TODO: Only show the first filename for now -->
+                    <!-- Leading slashes in *nix paths need to be trimmed to not mess up RTL -->
+                    <div class="font-mono grow bg-surface-50-900-token px-2 py-1 col-span-9 truncate text-end" dir="rtl">{trimLeftChar(tracks[page.page].filenames[0], "/")}</div>
+                </div>
+            {/if}
+            <hr class="!border-t-4 !border-dashed" />
+            <InfoSegment header="Integrity" value={tracks[page.page].test_and_copy.integrity} icon={IconDoubleInteger} />
+            <InfoSegment header="Integrity (skip zeroes)" value={tracks[page.page].test_and_copy.integrity_skipzero} icon={IconDoubleInteger} />
+            
+            {#if tracks[page.page].test_and_copy.integrity === 'Match' }
+                <ChecksumSegment header="T&C hash" hash={tracks[page.page].test_and_copy.test_hash} icon={IconHashtag} status={tracks[page.page].test_and_copy.integrity} />
+            {:else}
+                <ChecksumSegment header="Test hash" hash={tracks[page.page].test_and_copy.test_hash} icon={IconHashtag} status={tracks[page.page].test_and_copy.integrity} />
+                <ChecksumSegment header="Copy hash" hash={tracks[page.page].test_and_copy.copy_hash} icon={IconHashtag} status={tracks[page.page].test_and_copy.integrity} />
+            {/if}
 
-        {#if tracks[page.page].test_and_copy.integrity_skipzero === 'Match' }
-            <ChecksumSegment header="T&C hash (skip zeroes)" hash={tracks[page.page].test_and_copy.test_skipzero_hash} icon={IconHashtag} status={tracks[page.page].test_and_copy.integrity_skipzero} />
-        {:else}
-        <ChecksumSegment header="Test hash (skip zeroes)" hash={tracks[page.page].test_and_copy.test_skipzero_hash} icon={IconHashtag} status={tracks[page.page].test_and_copy.integrity_skipzero} />
-        <ChecksumSegment header="Copy hash (skip zeroes)" hash={tracks[page.page].test_and_copy.copy_skipzero_hash} icon={IconHashtag} status={tracks[page.page].test_and_copy.integrity_skipzero} />
+            {#if tracks[page.page].test_and_copy.integrity_skipzero === 'Match' }
+                <ChecksumSegment header="T&C hash (skip zeroes)" hash={tracks[page.page].test_and_copy.test_skipzero_hash} icon={IconHashtag} status={tracks[page.page].test_and_copy.integrity_skipzero} />
+            {:else}
+            <ChecksumSegment header="Test hash (skip zeroes)" hash={tracks[page.page].test_and_copy.test_skipzero_hash} icon={IconHashtag} status={tracks[page.page].test_and_copy.integrity_skipzero} />
+            <ChecksumSegment header="Copy hash (skip zeroes)" hash={tracks[page.page].test_and_copy.copy_skipzero_hash} icon={IconHashtag} status={tracks[page.page].test_and_copy.integrity_skipzero} />
+            {/if}
+        </div>
+        {#if Object.keys(tracks[page.page].errors).length > 0}
+            <hr class="my-4 !border-t-4 !border-dashed" />
+            <div class="flex items-center"><IconWarningAlt /><span class="ml-1 dark:font-light text-sm">Track Errors</span></div>
+            <Accordion regionPanel="space-y-0" padding="px-2 py-1" class="mt-2">
+                {#each Object.keys(tracks[page.page].errors) as errorType}
+                    <AccordionItem>
+                        <svelte:fragment slot="lead">
+                            <IconCheckmarkFilledError />
+                        </svelte:fragment>
+                        <svelte:fragment slot="summary">
+                            <div class="flex justify-between items-center">
+                                <span class="first-letter:capitalize text-sm">{errorType}</span>
+                                <span class="chip variant-soft-error rounded-full">{tracks[page.page].errors[errorType].count}</span>
+                            </div>
+                        </svelte:fragment>
+                        <svelte:fragment slot="content">
+                            {#if tracks[page.page].errors[errorType].ranges.length > 0}
+                                {#each tracks[page.page].errors[errorType].ranges as errorRange}
+                                    <div class="flex justify-between items-center">
+                                        <div>
+                                            <span class="text-xs variant-soft-primary rounded-full py-1 px-2 uppercase">Start</span>
+                                            <span class="text-xs">{secondsToMMSS(parseFloat(errorRange.start))}</span>
+                                        </div>
+                                        {#if errorRange.length}
+                                            <hr class="mx-2 grow !border-b-2 !border-dotted" />
+                                            <div>
+                                                <span class="text-xs">{secondsToMMSS(parseFloat(errorRange.start) + parseFloat(errorRange.length))}</span>
+                                                <span class="text-xs variant-soft-primary rounded-full py-1 px-2 uppercase">End</span>
+                                            </div>
+                                        {/if}
+                                    </div>
+                                {/each}
+                            {:else}
+                                <span class="text-xs">Position data not available/applicable.</span>
+                            {/if}
+                        </svelte:fragment>
+                    </AccordionItem>
+                {/each}
+            </Accordion>
         {/if}
     </div>
-    {#if Object.keys(tracks[page.page].errors).length > 0}
-        <hr class="my-4 !border-t-4 !border-dashed" />
-        <div class="flex items-center"><IconWarningAlt /><span class="ml-1 dark:font-light text-sm">Track Errors</span></div>
-        <Accordion regionPanel="space-y-0" padding="px-2 py-1" class="mt-2">
-            {#each Object.keys(tracks[page.page].errors) as errorType}
-                <AccordionItem>
-                    <svelte:fragment slot="lead">
-                        <IconCheckmarkFilledError />
-                    </svelte:fragment>
-                    <svelte:fragment slot="summary">
-                        <div class="flex justify-between items-center">
-                            <span class="first-letter:capitalize text-sm">{errorType}</span>
-                            <span class="chip variant-soft-error rounded-full">{tracks[page.page].errors[errorType].count}</span>
-                        </div>
-                    </svelte:fragment>
-                    <svelte:fragment slot="content">
-                        {#if tracks[page.page].errors[errorType].ranges.length > 0}
-                            {#each tracks[page.page].errors[errorType].ranges as errorRange}
-                                <div class="flex justify-between items-center">
-                                    <div>
-                                        <span class="text-xs variant-soft-primary rounded-full py-1 px-2 uppercase">Start</span>
-                                        <span class="text-xs">{secondsToMMSS(parseFloat(errorRange.start))}</span>
-                                    </div>
-                                    {#if errorRange.length}
-                                        <hr class="mx-2 grow !border-b-2 !border-dotted" />
-                                        <div>
-                                            <span class="text-xs">{secondsToMMSS(parseFloat(errorRange.start) + parseFloat(errorRange.length))}</span>
-                                            <span class="text-xs variant-soft-primary rounded-full py-1 px-2 uppercase">End</span>
-                                        </div>
-                                    {/if}
-                                </div>
-                            {/each}
-                        {:else}
-                            <span class="text-xs">Position data not available/applicable.</span>
-                        {/if}
-                    </svelte:fragment>
-                </AccordionItem>
-            {/each}
-        </Accordion>
-    {/if}
-</div>
+{/if}
