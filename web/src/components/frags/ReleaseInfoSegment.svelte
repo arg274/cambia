8947e4a65033fc5309b36c0be99460e3956edc92
arg274 <4648027+arg274@users.noreply.github.com>
2024-03-01 18:29:11 +0600

    Indicate if there are multiple releases for a disc ID

--- web/src/components/frags/ReleaseInfoSegment.svelte
+++ web/src/components/frags/ReleaseInfoSegment.svelte
@@ -1,54 +1,64 @@
 <script lang="ts">
 	import { getCaaCovers, getJoinedArtists } from "$lib/api/MusicBrainzApi";
 	import { castToType } from "$lib/utils";
-	import type { ICoverInfo, IRelease } from "musicbrainz-api";
+	import type { ICoverInfo, IImage, IRelease } from "musicbrainz-api";
 	import MusicBrainz from "../icons/MusicBrainz.svelte";
 	import type { ReleaseInfo } from "$lib/types/ReleaseInfo";
 	import classNames from "classnames";
+	import { onMount } from "svelte";
 
-    export let rel: IRelease | null = null;
+    export let rels: IRelease[] = [];
     export let relLog: ReleaseInfo;
     export let pending: boolean = false;
+    export let mbzTocId: string = "";
 
-    function castToCoverInfo(coverInfo: any): ICoverInfo {
-        return castToType<ICoverInfo>(coverInfo);
-    }
+    let frontImages: IImage[] = [];
+    let errored: boolean = false;
 
     const placeholder = "placeholder rounded-none";
     const imgDim = "w-[100px] h-[100px]";
     const animate = "animate-pulse";
     const imgError = placeholder;
+
+    function castToCoverInfo(coverInfo: any): ICoverInfo {
+        return castToType<ICoverInfo>(coverInfo);
+    }
+
+    onMount(async () => {
+        if (!rels) {
+            errored = true;
+            return;
+        }
+
+        try {
+            const coverRes = await getCaaCovers(rels[0].id);
+            const coverData = await coverRes.json();
+            frontImages = castToCoverInfo(coverData).images;
+
+            if (frontImages.length == 0) {
+                errored = true;
+            }
+        } catch {
+            errored = true;
+        }
+    });
 </script>
 
-<div class="flex flex-col">
+<div class="flex flex-col gap-2">
     <div class="flex mt-2 justify-start">
         <div class="flex items-center">
-            {#if rel}
-                {#await getCaaCovers(rel.id)}
-                    <div class={classNames(placeholder, imgDim, animate)}></div>
-                {:then coverRes}
-                    {#await coverRes.json()}
-                        <div class={classNames(placeholder, imgDim, animate)}></div>
-                    {:then coverData}
-                        {@const frontImages = castToCoverInfo(coverData).images}
-                        {#if frontImages.length > 0}
-                            <img class={imgDim} alt="{relLog.title} by {relLog.artist}" src={frontImages[0].thumbnails.small} />
-                        {:else}
-                            <div class={classNames(imgError, imgDim)}></div>
-                        {/if}
-                    {:catch}
-                        <div class={classNames(imgError, imgDim)}></div>
-                    {/await}
-                {:catch}
-                    <div class={classNames(imgError, imgDim)}></div>
-            {/await}
-            {:else}
+            {#if errored}
                 <div class={classNames(imgError, imgDim)}></div>
+            {:else if frontImages.length > 0}
+                <img class={imgDim} alt="{relLog.title} by {relLog.artist}" src={frontImages[0].thumbnails.small} />
+            {:else}
+                <div class={classNames(placeholder, imgDim, animate)}></div>
             {/if}
             <div class="ml-4 flex flex-col">
-                {#if rel}
-                    <span class="text-md">{getJoinedArtists(rel['artist-credit'])}</span>
-                    <span class="text-2xl font-bold">{rel.title}</span>
+                {#if rels.length > 0}
+                    <!-- Only using the first release -->
+                    <span class="text-md">{getJoinedArtists(rels[0]['artist-credit'])}</span>
+                    <span class="text-2xl font-bold">{rels[0].title}</span>
                 {:else if pending}
                     <div class="{classNames(placeholder, animate)} h-4 w-8"></div>
                     <div class="{classNames(placeholder, animate)} h-8 w-20 mt-2"></div>
@@ -64,9 +74,13 @@
             <div class="min-w-4">
                 <MusicBrainz />
             </div>
-            {#if rel}
-                <a type="button" class="self-end" href="https://musicbrainz.org/release/{rel.id}" target="_blank">
-                        <span class="text-sm font-mono">{rel.id}</span>
+            {#if rels.length == 1}
+                <a type="button" class="self-end" href="https://musicbrainz.org/release/{rels[0].id}" target="_blank">
+                    <span class="text-sm font-mono">{rels[0].id.split("-")[0]}</span>
+                </a>
+            {:else if rels.length > 1}
+                <a type="button" class="self-end" href="https://musicbrainz.org/cdtoc/{mbzTocId}" target="_blank">
+                    <span class="text-sm font-mono">MULTIPLE</span>
                 </a>
             {:else if pending}
                 <div class="{classNames(placeholder, animate)} h-4 w-72"></div>
