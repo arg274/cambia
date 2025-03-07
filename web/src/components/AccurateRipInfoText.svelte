2b5555e2d4ee8c37f8e89123147529e5b02f19e0
arg274 <4648027+arg274@users.noreply.github.com>
2025-03-07 18:34:01 +0600

    Fix UI crash when AR info is not available

--- web/src/components/AccurateRipInfoText.svelte
+++ web/src/components/AccurateRipInfoText.svelte
@@ -1,24 +1,33 @@
 <script lang="ts">
-    import type { TrackEntry } from "$lib/types/TrackEntry";
-	import AccurateRipChecksumSegment from "./frags/AccurateRipChecksumSegment.svelte";
-	import InfoSegment from "./frags/InfoSegment.svelte";
+	import type { TrackEntry } from '$lib/types/TrackEntry';
+	import AccurateRipChecksumSegment from './frags/AccurateRipChecksumSegment.svelte';
+	import InfoSegment from './frags/InfoSegment.svelte';
 
-    export let track: TrackEntry;
+	export let track: TrackEntry;
 
-    $: ar = track.ar_info[track.ar_info.length - 1];
+	$: ar = track.ar_info[track.ar_info.length - 1];
 </script>
+
 <div class="flex flex-col gap-4">
-    <div class="flex gap-2 items-center">
-        <h6>Track {track.num}</h6>
-        {#if track.aborted}
-            <div class="variant-soft-error rounded-md text-xs px-2 py-1">Aborted</div>
-        {/if}
-    </div>
-    <div class="flex flex-col gap-4">
-        <InfoSegment header="Status" value={ar.status} />
-        <AccurateRipChecksumSegment header="Signature" sign={ar.sign} />
-        {#if ar.sign !== ar.offset_sign}
-            <AccurateRipChecksumSegment header="Signature" sign={ar.offset_sign} arOffset={ar.confidence?.offset} />
-        {/if}
-    </div>
+	<div class="flex gap-2 items-center">
+		<h6>Track {track.num}</h6>
+		{#if track.aborted}
+			<div class="variant-soft-error rounded-md text-xs px-2 py-1">Aborted</div>
+		{/if}
+	</div>
+	<div class="flex flex-col gap-4">
+		{#if ar}
+			<InfoSegment header="Status" value={ar.status} />
+			<AccurateRipChecksumSegment header="Signature" sign={ar.sign} />
+			{#if ar.sign !== ar.offset_sign}
+				<AccurateRipChecksumSegment
+					header="Signature"
+					sign={ar.offset_sign}
+					arOffset={ar.confidence?.offset}
+				/>
+			{/if}
+		{:else}
+			<div class="text-xl font-bold">No AR Data</div>
+		{/if}
+	</div>
 </div>
